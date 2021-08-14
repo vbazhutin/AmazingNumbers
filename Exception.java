06aede8f9004222e1a956d335e2f245d19c754a9
Vlad <enslaver45@gmail.com>
2021-08-14 17:42:03 -0400

    Added comments for better code reading

--- Exception.java
+++ Exception.java
@@ -7,22 +7,26 @@ public class Exception {
     public static boolean handleException() {
             String[] props = Input.getInput();
             String[][] wrongProps = Number.mutuallyExclusiveProps;
-            if (!props[0].matches("\\d+")) {
+
+            if (!props[0].matches("\\d+")) {  //The first number must be >= 0
                 System.out.println("\nThe first parameter should be a natural number or zero.");
                 return false;
 
-            } else if (props.length > 1 && !props[1].matches("\\d+")) {
+            } else if (props.length > 1 && !props[1].matches("\\d+")) { //The second num must be > 0
                 System.out.println("\nThe second parameter should be a natural number.");
                 return false;
 
-            } else if (props.length > 2) {
+            } else if (props.length > 2) {  //checking the value of properties in input
                 for (int i = 0; i < wrongProps.length; i++) {
                     int count = 0;
                     for (int j = 0; j < wrongProps[i].length; j++) {
+                        //looking for mutually exclusive props
                         if (Arrays.toString(props).toUpperCase().contains(wrongProps[i][j])) {
                             props[j] = wrongProps[i][j];
                             count++;
                         }
+                        //if one of the mutually exclusive props found in the input ->
+                        //return false, output the found props
                         if (count == 2) {
                             System.out.printf("The request contains mutually exclusive properties: [%s, %s]\n" +
                                     "There are no numbers with these properties.", props[0], props[1]);
@@ -33,13 +37,14 @@ public class Exception {
 
                 StringBuilder str = new StringBuilder();
                 for (int i = 2; i < props.length; i++) {
+                    //looking for invalid props and putting them into a separate StringBuilder var
                     if (!Arrays.toString(Number.numberProperties).contains(props[i].toUpperCase())) {
                         str.append(props[i].toUpperCase()).append(", ");
                     }
                 }
 
                 String[] invalidProps = str.toString().split(", ");
-
+                //if one invalid property found -> output it and return false
                 if (invalidProps.length == 1 && !Arrays.toString(Number.numberProperties)
                         .contains(invalidProps[0].toUpperCase())) {
                     System.out.printf("The property [" +
@@ -49,6 +54,7 @@ public class Exception {
                     return false;
                 }
 
+                //if many invalid properties found -> output them and return false
                 else if (str.length() > 0) {
                     System.out.printf("The properties [" +
                                     str.replace(str.length() - 2, str.length(), "]")
