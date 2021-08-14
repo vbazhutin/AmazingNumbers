fc96941405a4f2a219e14f72d31962e1fae7cd34
Vlad <enslaver45@gmail.com>
2021-08-14 17:29:45 -0400

    Added comments for better code reading

--- Input.java
+++ Input.java
@@ -9,7 +9,7 @@ public class Input {
     public static void inputProps() {
         Scanner in = new Scanner(System.in);
         System.out.println("\nEnter a request:");
-        input = in.nextLine().split("\\s");
+        input = in.nextLine().split("\\s");  //making input an array of Strings
     }
 
     public static String[] getInput() {
