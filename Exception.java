96378e651c6a6b49797a998dcf7908603026eb1e
Vlad <enslaver45@gmail.com>
2021-08-14 22:57:20 -0400

    Fixed an error and added more functionality to mutually exclusive props in exception handling

--- Exception.java
+++ Exception.java
@@ -16,20 +16,25 @@ public class Exception {
                 System.out.println("\nThe second parameter should be a natural number.");
                 return false;
 
-            } else if (props.length > 2) {  //checking the value of properties in input
-                for (int i = 0; i < wrongProps.length; i++) {
+            } else if (props.length > 2) {
+                //checking the value of properties in input
+                //if found one, make sure it's not mutually exclusive
+                for (int j = 0; j < wrongProps.length; j++) {
+                    String[] invalidProps = new String[2];
                     int count = 0;
-                    for (int j = 0; j < wrongProps[i].length; j++) {
-                        //looking for mutually exclusive props
-                        if (Arrays.toString(props).toUpperCase().contains(wrongProps[i][j])) {
-                            props[j] = wrongProps[i][j];
-                            count++;
+                    //comparing every possible correct prop with the input
+                    for (int k = 0; k < wrongProps[j].length; k++) {
+                        for (int i = 0; i < props.length; i++) {
+                            //if found incorrect -> assign to element of array
+                            if (props[i].equalsIgnoreCase(wrongProps[j][k])) {
+                                invalidProps[k] = wrongProps[j][k];
+                                count++;
+                            }
                         }
-                        //if one of the mutually exclusive props found in the input ->
-                        //return false, output the found props
+                        //output mutually exclusive props
                         if (count == 2) {
                             System.out.printf("The request contains mutually exclusive properties: [%s, %s]\n" +
-                                    "There are no numbers with these properties.", props[0], props[1]);
+                                    "There are no numbers with these properties.", invalidProps[0], invalidProps[1]);
                             return false;
                         }
                     }
@@ -38,19 +43,19 @@ public class Exception {
                 StringBuilder str = new StringBuilder();
                 for (int i = 2; i < props.length; i++) {
                     //looking for invalid props and putting them into a separate StringBuilder var
-                    if (!Arrays.toString(Number.numberProperties).contains(props[i].toUpperCase())) {
+                    if (!Arrays.toString(Number.numberProps).contains(props[i].toUpperCase())) {
                         str.append(props[i].toUpperCase()).append(", ");
                     }
                 }
 
                 String[] invalidProps = str.toString().split(", ");
                 //if one invalid property found -> output it and return false
-                if (invalidProps.length == 1 && !Arrays.toString(Number.numberProperties)
+                if (invalidProps.length == 1 && !Arrays.toString(Number.numberProps)
                         .contains(invalidProps[0].toUpperCase())) {
                     System.out.printf("The property [" +
                                     str.replace(str.length() - 2, str.length(), "]")
                                             .append(" is wrong\nAvailable properties: %s"),
-                            Arrays.toString(Number.numberProperties));
+                            Arrays.toString(Number.numberProps));
                     return false;
                 }
 
@@ -59,7 +64,7 @@ public class Exception {
                     System.out.printf("The properties [" +
                                     str.replace(str.length() - 2, str.length(), "]")
                                             .append(" are wrong\nAvailable properties: %s"),
-                            Arrays.toString(Number.numberProperties));
+                            Arrays.toString(Number.numberProps));
                     return false;
                 }
             }
