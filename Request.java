ca809b31d0307c539cdf2466a89556d4595ba417
Vlad <enslaver45@gmail.com>
2021-08-14 22:59:07 -0400

    Added 2 more cases since the max input can be 12 props

--- Request.java
+++ Request.java
@@ -22,11 +22,13 @@ public class Request {
             case 8:
             case 9:
             case 10:
+            case 11:
+            case 12:
                 Properties.processProperties(Long.parseLong(args[0]),
                         Long.parseLong(args[1]), Arrays.copyOfRange(args, 2, args.length));
                 break;
             default:
-                System.out.println("Number of properties can't be more than 10");
+                System.out.println("Number of properties can't be more than 12");
                 break;
         }
     }
